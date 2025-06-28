367d38db580b01e7b134e9dc8470a19ffb8bccd2
Pratham Darji <155282402+Pratham-Prog861@users.noreply.github.com>
2025-06-28 11:41:52 +0530

    Update package dependencies and refactor server setup to use Express.

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const io = require('socket.io')(3000)
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+// Serve static files
+app.use(express.static('./'))
 
 const users = {}
 
@@ -14,4 +21,9 @@ io.on('connection', socket => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
   })
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
 })
